5e061e8e4b3825c2722ca4d3c1cadb5f804c6121
Alvaro Nicolas Ortiz <160544793+ANicolasOrtiz@users.noreply.github.com>
2024-06-14 19:52:44 +0000

    ejercicio parte 1

---
+++ src/js/component/Timer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { BsStopwatch } from "react-icons/bs";
+
+
+ export const Timer = (miliseconds, seconds, minutes, hours, changeSeconds, changeMinutes, changeHours) => {
+    return (
+        <div className="TimeWrapper">
+            <BsStopwatch />
+            <div className="d-flex flex-column">
+                <label>hh</label>
+                <input value={hours} onChange={changeHours} />
+            </div>
+            <div className="d-flex flex-column">
+                <label>mm</label>
+                <input value={minutes} onChange={changeMinutes} />
+            </div>
+            <div className="d-flex flex-column">
+                <label>ss</label>
+                <input value={seconds} onChange={changeSeconds} />
+            </div>
+            <div className="d-flex flex-column">
+                <label>ms</label>
+                <input value={miliseconds}/>
+            </div>
+        </div>
+    )
+}
