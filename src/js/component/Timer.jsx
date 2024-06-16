613790e78ff59438771b3d7170acb50ad218310d
Alvaro Nicolas Ortiz <160544793+ANicolasOrtiz@users.noreply.github.com>
2024-06-16 22:57:18 +0000

    ejercicio terminado

--- src/js/component/Timer.jsx
+++ src/js/component/Timer.jsx
@@ -1,24 +1,28 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
- export const Timer = (miliseconds, seconds, minutes, hours, changeSeconds, changeMinutes, changeHours) => {
+
+ export const Timer = ({thousandsDigit, hundredsDigit, tensDigit, onesDigit}) => {
     return (
-        <div className="TimeWrapper">
-            <div className="d-flex flex-column">
-                <label>hh</label>
-                <input value={hours} onChange={changeHours} />
-            </div>
-            <div className="d-flex flex-column">
-                <label>mm</label>
-                <input value={minutes} onChange={changeMinutes} />
-            </div>
-            <div className="d-flex flex-column">
-                <label>ss</label>
-                <input value={seconds} onChange={changeSeconds}/>
-            </div>
-            <div className="d-flex flex-column">
-                <label>ms</label>
-                <input value={miliseconds}/>
-            </div>
+       <div className="TimeWrapper">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clock" viewBox="0 0 16 16">
+             <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z"/>
+             <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0"/>
+         </svg>
+            <div className="seconds thousands">{thousandsDigit}</div>
+            <div className="seconds hundreds">{hundredsDigit}</div>
+            <div className="seconds tens">{tensDigit}</div>
+            <div className="seconds ones">{onesDigit}</div>
         </div>
     )
 }
+
+Timer.propTypes = {
+    thousandsDigit: PropTypes.number,
+    hundredsDigit: PropTypes.number,
+    tensDigit: PropTypes.number,
+    onesDigit: PropTypes.number,
+}
+
+
+export default Timer;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {Timer} from "./Timer"
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-import { CountDownTimer } from "./CountDownTimer";
-
-//create your first component
 const Home = () => {
+
+	const [counter, setCounter] = useState(0);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setCounter(counter => counter + 1)
+		}, 1000)
+
+		return () => clearInterval(interval)
+	}, [counter])
+
+
+	function calculateSeconds(aCounter, placeValue){
+		return Math.floor(aCounter / placeValue) % 10
+	}
+
+
 	return (
 		<div className="text-center">
-			<CountDownTimer />
+			<Timer 
+			thousandsDigit = {calculateSeconds(counter, 1000)}
+			hundredsDigit = {calculateSeconds(counter, 100)}
+			tensDigit = {calculateSeconds(counter, 10)}
+			onesDigit = {calculateSeconds(counter, 1)}
+			/>
 		</div>
 	);
 };
